8886db5ae7939fefc0f1e9c161af119468cbf69b
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-01-28 12:58:46 -0800

    data populates within results section and search bar functionality set

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // Define global variables up here
 
-let searchInput = document.getElementById("search-bar");
+
 
 // Use this filter to change all letters in string to uppercase if needed.
 // searchInput.value.toUppercase();
@@ -8,48 +8,75 @@ let searchInput = document.getElementById("search-bar");
 
 // fetch requests
 // create fetch requests that get weather info from open weather and populate them into their respective HTML areas
-function search() {
+function search(searchTerm) {
 
 // fetch current weather conditions for search city
-fetch("https://api.openweathermap.org/data/2.5/weather?q=london&units=imperial&appid=191533fb1cf9b39f93e215abebe1b6c9")
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=imperial&appid=191533fb1cf9b39f93e215abebe1b6c9`)
 .then(function(response){
     return response.json();
 })
 .then(function(data){
-    console.log(data);
     // get name of city
-    console.log(data.name); 
     document.getElementById('name').innerHTML = data.name;
     // pass in temperature value
-    console.log(data.main.temp);
     document.getElementById('temp').innerHTML = Math.floor(data.main.temp);
     // pass in humidity value
-    console.log(data.main.humidity);
     document.getElementById('hum').innerHTML = data.main.humidity;
     // pass in wind speed
-    console.log(data.wind.speed);
     document.getElementById('wind').innerHTML = data.wind.speed;
+
     // set variables for lat and long to be used in next call
     let lon = data.coord.lon;
     let lat = data.coord.lat;
-});
-// fetch results for UV Index using One Call API.  
-console.log(lat, lon);
-fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=191533fb1cf9b39f93e215abebe1b6c9`)
-// fetch results for the 5 day forecast cards
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=london&appid=191533fb1cf9b39f93e215abebe1b6c9")
 
+        // fetch results for UV Index using One Call API.  
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=191533fb1cf9b39f93e215abebe1b6c9`)
+        .then(function(response){
+        return response.json();
+        })
+        .then(function(data){
+        document.getElementById('uv').innerHTML = data.current.uvi;
+        })
+
+    // fetch results for the 5 day forecast cards
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=191533fb1cf9b39f93e215abebe1b6c9&units=imperial`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let fiveDayArray = data.list.filter(day => day.dt_txt.includes('12:00:00'));
+        
+        for(let i = 0; i < fiveDayArray.length; i++){
+        let dateCard1 = new Date(fiveDayArray[i].dt_txt).toLocaleString().split(',')[0];
+        document.getElementById(`date-card-${i}`).innerHTML = dateCard1;
+        let iconId = fiveDayArray[i].weather[0].icon;
+        document.getElementById(`img-card-${i}`).setAttribute("src", `http://openweathermap.org/img/wn/${iconId}@2x.png`);
+        document.getElementById(`temp-card-${i}`).innerHTML =  Math.floor(fiveDayArray[i].main.temp);
+        document.getElementById(`hum-card-${i}`).innerHTML = fiveDayArray[i].main.humidity;
+
+        }
+
+    })
+
+    
+})
 
 // end search function
 }
 
-// write content with fetched variables
 
-// local storage saves last search so that localtion is presented when the page is reloaded.  Hardcode a popular city to present on load by setting an intial value for the searchh term and then replace that with user's input.
 
 // search button engages the fetch requests and clears text from search input
-document.getElementById('search-button').addEventListener("click", search);
+document.getElementById('search-button').addEventListener("click", function(){
+    let searchInput = document.getElementById("search-input");
+    let searchTerm = searchInput.value.toLowerCase().trim();
+    search(searchTerm);
+    console.log(searchTerm);
+});
+
+// look up event listeners for UL of popular search terms.  callback search with parameter of text in list link 
 
 // popular cities list takes the name of the clicked city and enters it as a search term, then calls the search function for that city.
 
 
+// local storage saves last search so that location is presented when the page is reloaded.  Hardcode a popular city to present on load by setting an intial value for the searchh term and then replace that with user's input.
